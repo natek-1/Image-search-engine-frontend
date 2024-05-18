b7d76698bcdf757466a675a9e0d0431ed24a96c5
Nathan Gabriel <nategabrielk@icloud.com>
2024-05-18 18:58:00 -0400

    set up configs entity and docker ignore

---
+++ src/entity/config.py
@@ -0,0 +1,38 @@
+import from_root
+import os
+from dotenv import load_dotenv
+
+
+
+class s3Config:
+    def __init__(self):
+        self.ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
+        self.SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+        self.REGION = os.environ['AWS_REGION']
+        self.BUCKET = os.environ['AWS_BUCKET_NAME']
+        self.KEY = "model"
+        self.ZIP_NAME = "data.zip"
+        self.ZIP_PATHS = [(os.path.join(from_root(), "data", "embeddings", "embeddings.json"), "embeddings.json"),
+                          (os.path.join(from_root(), "data", "embeddings", "embeddings.ann"), "embeddings.ann"),
+                          (os.path.join(from_root(), "model", "finetuned", "model.pth"), "model.pth")]
+        
+    def get_s3config(self):
+        return self.__dict__
+    
+class PredictConfig:
+    def __init__(self):
+        self.NUM_LABEL = 101
+        self.REPO = "pytorch/vision:v0.13.0"
+        self.BASE_MODEL = "resnet18"
+        self.PRETRAINED = "ResNet18_Weights.DEFAULT"
+        self.IMAGE_SIZE = 256
+        self.EMBEDDING_SIZE = 256
+        self.SEARCH_MATRIX = 'euclidean'
+        self.NUM_PREDICTIONS = 20
+        self.PATH = os.path.join(from_root(), "artifacts")
+        self.MODEL_PATHS = [(os.path.join(from_root(), "artifacts", "embeddings.json"), "embeddings.json"),
+                          (os.path.join(from_root(), "artifacts", "embeddings.ann"), "embeddings.ann"),
+                          (os.path.join(from_root(), "model", "model.pth"), "model.pth")]
+    
+    def get_predict_config(self):
+        return self.__dict__
